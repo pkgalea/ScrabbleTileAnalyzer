b879cc951f4c296f106fce6f6178d0bdc3f0b4f0
= <=>
2020-01-05 17:52:47 -0600

    end of holidays

---
+++ RemoveChallenges.sql
@@ -0,0 +1,14 @@
+/*UPDATE turn SET challenged_away = FALSE;
+UPDATE turn set challenged_away = TRUE where turn_id in (Select lag_turn_id from (
+    SELECT move, turn_id, LAG (turn_id, 1)  OVER (PARTITION BY gamenum ORDER BY movenum) as lag_turn_id  from turn) as sub where move like '-chl-%') ;
+UPDATE turn SET is_challenge = FALSE;
+UPDATE turn SET is_challenge = TRUE where move like '-chl-%';
+UPDATE turn SET is_exchange = FALSE;
+UPDATE turn SET is_exchange = TRUE WHERE move = '---';
+*/
+
+UPDATE game set p1_final_score = p1_score, p2_final_score = p2_score from turn inner join (select gamenum, max(movenum) as max_movenum from turn where p1_score != 0 and p2_score != 0
+ group by gamenum) M on M.gamenum = turn.gamenum and M.max_movenum = turn.movenum WHERE game.gamenum = turn.gamenum
+
+
+
