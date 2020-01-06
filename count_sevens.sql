98075ce1ee52f04fa43a9d363750fb92681c6b3a
= <=>
2020-01-06 16:05:15 -0600

    some 7 sql stuff

---
+++ count_sevens.sql
@@ -0,0 +1,13 @@
+ select * from 
+ (
+ Select gamenum, 
+ (Select max(movenum) from turn where length(rack) = 7 and is_player2=' 1' and turn.gamenum = game.gamenum ) as p2_last_7,
+ (Select min(movenum) from turn where length(rack) < 7 and is_player2=' 1' and turn.gamenum = game.gamenum) as p2_first_non_7,
+ (Select max(movenum) from turn where length(rack) = 7 and is_player2=' 0' and turn.gamenum = game.gamenum ) as p1_last_7,
+ (Select min(movenum) from turn where length(rack) < 7 and is_player2=' 0' and turn.gamenum = game.gamenum) as p1_first_non_7 
+ 
+ from game
+) g where (g.p2_last_7 < p2_first_non_7) or (g.p1_last_7 < p1_first_non_7)
+ 
+ 
+ 
